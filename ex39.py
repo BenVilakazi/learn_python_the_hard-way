5e3024c0757337550a7b786c9103e9f239cd8c77
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-10-21 09:40:26 +0200

    Create ex39.py

---
+++ ex39.py
@@ -0,0 +1,20 @@
+# create a mapping of provinces to abbr provinces
+
+provinces { 'Northern Cape': 'NC',
+            'Eastern Cape': 'EC',
+            'Free State': 'FS',
+            'Western Cape': 'WC',
+            'Limpopo': 'LP',
+            'North Worth': 'NW',
+            'KwaZulu-Natal': 'KZN',
+            'Mpumalanga': 'MP',
+            'Gauteng': 'GP'
+          
+          }
+
+Cities { 'JHB': 'Johannesburg',
+         'DBN': 'Durban',
+         'PE': 'Port-Elizabeth'
+         'WK': 'Welkom'
+         '
+       }
