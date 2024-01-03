cb21204bc079b9883729c59cfcb6141ed3fd7370
Ben Vilakazi <xolanithengubane@gmail.com>
2024-01-03 14:37:01 +0200

    changes

--- ex39.py
+++ ex39.py
@@ -1,6 +1,6 @@
 # create a mapping of provinces to abbr provinces
 
-provinces { 'Northern Cape': 'NC',
+provinces = ['Northern Cape': 'NC',
             'Eastern Cape': 'EC',
             'Free State': 'FS',
             'Western Cape': 'WC',
@@ -10,11 +10,13 @@ provinces { 'Northern Cape': 'NC',
             'Mpumalanga': 'MP',
             'Gauteng': 'GP'
           
-          }
+]
+#create abasic set of provinces and cities
 
-Cities { 'JHB': 'Johannesburg',
+Cities = [ 'JHB': 'Johannesburg',
          'DBN': 'Durban',
-         'PE': 'Port-Elizabeth'
+         'PE': 'Port-Elizabeth',
          'WK': 'Welkom'
-         '
-       }
+]
+
+
